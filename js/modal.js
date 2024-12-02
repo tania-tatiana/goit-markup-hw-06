fa3c24dc80b17a967272658d6eb54d2d5e23649f
Tatiana <bangchanka0310@gmail.com>
2024-12-02 18:14:24 +0200

    add adaptive modal

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,41 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-opened на кнопку відкриття
+    openModalBtn: document.querySelector('[data-modal-opened]'),
+    // Додати атрибут data-modal-closed на кнопку закриття
+    closeModalBtn: document.querySelector('[data-modal-closed]'),
+    // Додати атрибут data-modalka на бекдроп модалки
+    modal: document.querySelector('[data-modalka]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    // Додати атрибут mobile-data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector('[mobile-data-modal-open]'),
+    // Додати атрибут mobile-data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[mobile-data-modal-close]'),
+    // Додати атрибут mobile-data-modal на бекдроп модалки
+    modal: document.querySelector('[mobile-data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle('is-open');
+  }
+})();
