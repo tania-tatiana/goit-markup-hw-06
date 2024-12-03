17446c5f1b3b00b3501e13e1c87eeae6736f1dad
Tatiana <bangchanka0310@gmail.com>
2024-12-03 15:15:33 +0200

    add mobile modal

--- js/modal.js
+++ js/modal.js
@@ -17,24 +17,7 @@
   }
 })();
 
-(() => {
-  const refs = {
-    // Додати атрибут data-modal-opened на кнопку відкриття
-    openModalBtn: document.querySelector('[data-modal-opened]'),
-    // Додати атрибут data-modal-closed на кнопку закриття
-    closeModalBtn: document.querySelector('[data-modal-closed]'),
-    // Додати атрибут data-modalka на бекдроп модалки
-    modal: document.querySelector('[data-modalka]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
-})();
 
 (() => {
   const refs = {
