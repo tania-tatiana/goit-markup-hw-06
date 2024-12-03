da9ef2456be5f236cbe49dc2478b04dac70b7c91
Tatiana <bangchanka0310@gmail.com>
2024-12-03 17:50:16 +0200

    add adaptive features section

--- js/mobile-modal.js
+++ js/mobile-modal.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[mobile-data-modal-open]'),
+    // Додати атрибут mobile-data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector('[mobile-data-modal-close]'),
+    // Додати атрибут mobile-data-modal на бекдроп модалки
+    modal: document.querySelector('[mobile-data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
